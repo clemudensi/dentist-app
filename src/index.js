4a2b25b4ff435aca544e69f6fd5320af454c523a
Oluwole Oyekanmi <wolexzie@gmail.com>
2017-05-06 17:46:39 +0100

    Structure exit file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 require('bootstrap/dist/css/bootstrap.css');
 
+import App from './App';
+import { HashRouter as Router, Route} from 'react-router-dom';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route exact path="/" component={App}/>
+        </Router>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <App />,
+  <Index />,
   document.getElementById('root')
 );
